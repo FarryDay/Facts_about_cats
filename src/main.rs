80a005d5f7de3379421d8d674ec47872b9eb0573
FarryDay <Mokrushin29@yandex.ru>
2024-03-31 12:21:30 +0300

    Deploy

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+struct Person {
+    name: String,
+    age: i32,
+    weight: f32
+}
+
+fn main() {
+    let person = Person{
+        name: String::from("Kirill"),
+        age: ,
+        weight: 66.0
+    };
+    let mut array = vec!();
+    for mut key in array {
+        println!("{key}")
+    }
+}
